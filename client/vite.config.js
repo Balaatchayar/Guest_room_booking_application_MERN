857ad5846c9e62642af793bcfe5630d2ab200e3d
BALA ATCHAYA R <117425696+Balaatchayar@users.noreply.github.com>
2024-11-19 12:57:23 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: 'dist', // Output directory for production files
-        target: 'esnext', // Optimized for modern browsers
+        outDir: 'dist', // Ensure the build output is in the 'dist' folder
     },
 });
